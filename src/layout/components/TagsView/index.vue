533eabf6c7ce1022ac5edbe0dd6f879da1b9243b
Mochen07 <mochen7777@gmail.com>
2019-11-21 22:41:45 +0800

    modify

--- src/layout/components/TagsView/index.vue
+++ src/layout/components/TagsView/index.vue
@@ -1,17 +1,13 @@
 <template>
   <div id="tags-view-container" class="tags-view-container">
-    <scroll-pane ref="scrollPane" class="tags-view-wrapper">
-      tags-view
-    </scroll-pane>
+    tags-view
   </div>
 </template>
 
 <script>
-import ScrollPane from './ScrollPane'
 
 export default {
-  name: 'TagsView',
-  components: { ScrollPane }
+  name: 'TagsView'
 }
 </script>
 

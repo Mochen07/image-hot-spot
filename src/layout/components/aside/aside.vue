4135cebf22cdb454417f3806efa8230818ca7cf5
Mochen07 <mochen7777@gmail.com>
2019-12-26 07:42:28 +0800

    modify minor

--- src/layout/components/aside/aside.vue
+++ src/layout/components/aside/aside.vue
@@ -1,41 +1,39 @@
 <template>
-  <div id="aside" class="aside">
-    <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-      <el-menu
-        default-active="0"
-        :default-openeds="['1']"
-      >
-        <el-menu-item index="0" @click="routerLinks('/')">
-          <i class="el-icon-s-home" />
-          <span slot="title">Home</span>
+  <el-aside id="aside" class="aside" width="200px">
+    <el-menu
+      default-active="0"
+      :default-openeds="['1']"
+    >
+      <el-menu-item index="0" @click="routerLinks('/')">
+        <i class="el-icon-s-home" />
+        <span slot="title">Home</span>
+      </el-menu-item>
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-picture-outline-round" />
+          <span>Feature</span>
+        </template>
+        <el-menu-item index="1-1" @click="routerLinks('/feature/image-hot-area')">
+          Image Hot Area
         </el-menu-item>
-        <el-submenu index="1">
-          <template slot="title">
-            <i class="el-icon-picture-outline-round" />
-            <span>Feature</span>
-          </template>
-          <el-menu-item index="1-1" @click="routerLinks('/feature/image-hot-area')">
-            Image Hot Area
-          </el-menu-item>
-          <el-menu-item index="1-2" @click="addTabsView">
-            Tabs View
-          </el-menu-item>
-        </el-submenu>
-        <el-submenu index="2">
-          <template slot="title">
-            <i class="el-icon-message" />
-            <span>Element</span>
-          </template>
-          <el-menu-item index="2-1" @click="routerLinks('/element/table')">
-            Table
-          </el-menu-item>
-          <el-menu-item index="2-2" @click="routerLinks('/element/form')">
-            Form
-          </el-menu-item>
-        </el-submenu>
-      </el-menu>
-    </el-aside>
-  </div>
+        <el-menu-item index="1-2" @click="addTabsView">
+          Tabs View
+        </el-menu-item>
+      </el-submenu>
+      <el-submenu index="2">
+        <template slot="title">
+          <i class="el-icon-message" />
+          <span>Element</span>
+        </template>
+        <el-menu-item index="2-1" @click="routerLinks('/element/table')">
+          Table
+        </el-menu-item>
+        <el-menu-item index="2-2" @click="routerLinks('/element/form')">
+          Form
+        </el-menu-item>
+      </el-submenu>
+    </el-menu>
+  </el-aside>
 </template>
 
 <script>
@@ -56,5 +54,7 @@ export default {
 </script>
 
 <style scoped lang="scss" ref="stylesheet/scss">
-.aside{}
+.aside{
+  background-color: rgb(238, 241, 246)
+}
 </style>

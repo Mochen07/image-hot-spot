03d6b8d3e480249da81cc4f510a3a8f13019aa7f
Mochen07 <mochen7777@gmail.com>
2020-03-23 21:47:03 +0800

    add emoji page

---
+++ src/views/feature/emoji.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="emoji">
+    <EmojiBox
+      @skinChange="skinChange"
+    />
+  </div>
+</template>
+
+<script>
+import EmojiBox from '@/components/plugin/emoji/emoji'
+export default {
+  name: 'Emoji',
+  components: { EmojiBox },
+  methods: {
+    skinChange(item) {
+      this.$notify({
+        title: '表情',
+        message: item,
+        type: 'success'
+      })
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss" ref="stylesheet/scss">
+.emoji{
+  display: flex; justify-content: center;
+}
+</style>
